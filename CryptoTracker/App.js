c98178eb198afd2f30077b65ed5e7a7b368132df
William Williams <Will@Hannibal-Barca.local>
2018-04-13 03:15:24 -0600

    Initial commit of the cryptotracker project

---
+++ CryptoTracker/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+import { Header, CryptoContainer } from './src/components';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={Store}>
+        <View style={styles.container}>
+          <Header/>
+          <CryptoContainer />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ CryptoTracker/src/Actions/FetchCoinData.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { apiBaseURL } from '../Utils/Constants';
+import {FETCHING_COIN_DATA, 
+    FETCHING_COIN_DATA_SUCCESS, 
+    FETCHING_COIN_DATA_FAIL} from '../Utils/ActionTypes';
+
+export default function FetchCoinData() {
+    return dispatch => {
+        dispatch({ type: FETCHING_COIN_DATA })
+
+        const instance = axios.create({
+            baseURL: '{apiBaseURL}/v1/ticker/?limit=10',
+            timeout: 1000,
+            headers: {'X-Custom-Header': 'foobar'}
+          });
+
+        return instance.get("http://api.coinmarketcap.com/v1/ticker/?limit=10")
+            .then(res => {
+                return dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data });
+            })
+            .catch(err => {
+                console.log('There has been a problem with your fetch operation: ' + err.message);
+
+                return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err});
+            });
+    }
+}
